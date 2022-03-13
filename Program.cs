828e47b06bfe6883c8f4a0108896c16a130f20bd
MdAsfaqueAnsari <101329100+MdAsfaqueAnsari@users.noreply.github.com>
2022-03-13 15:20:14 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grade
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the 1st marks");
+            int h=int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the 2nd marks");
+            int m = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the 3rd marks");
+            int e=int.Parse(Console.ReadLine());
+
+            int total=h+m+e;
+            float per = total * 100 / 300;
+            if (total >= 200)
+            {
+                Console.WriteLine("grade A");
+            }
+            else if (total >= 150)
+            {
+                Console.WriteLine("grade B");
+            }
+            else
+            {
+                Console.WriteLine("fail");
+            }
+
+
+            Console.WriteLine("1st marks:- {0} 2nd marks :- {1} 3rd marks:- {2} total:- {3} per:- {4}", h, m, e, total, per);
+
+            Console.ReadLine();
+        }
+    }
+}
