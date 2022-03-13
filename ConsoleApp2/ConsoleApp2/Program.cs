ea7bbd6893704f5b358cbbcf01b52f841dee74ff
MdAsfaqueAnsari <101329100+MdAsfaqueAnsari@users.noreply.github.com>
2022-03-13 15:13:20 +0530

    Add files via upload

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+class IfElse
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter your Age");
+        int age = int.Parse(Console.ReadLine());
+        if (age >= 18)
+        {
+            Console.WriteLine("Enter your choice");
+            string choice = Console.ReadLine();
+            if (choice == "y")
+            {
+                Console.WriteLine("Vote for Eligible");
+            }
+            else
+            {
+                Console.WriteLine("Get your voter card");
+            }
+        }
+    }
+}
---
+++ Interface/Interface/Interface.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface
+{
+    interface ICar
+    {
+        void Speed();
+    }
+    class Ab:ICar
+    {
+        public void Speed()
+        {
+            Console.WriteLine(  "Speed is the "  );
+        }
+    }
+    internal class Interface
+    {
+        public static void Main (string[] args)
+        {
+          ICar obj=new Ab();
+            obj.Speed();
+          Console.ReadLine();
+        }
+    }
+}
